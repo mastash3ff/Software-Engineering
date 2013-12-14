5fa49ff9e83f2d162be0d0dafd1d52a58ba302f0
mastash3ff <bsheffield2008@yahoo.com>
2013-12-14 01:06:35 -0600

    Added in reading from Reddit.

---
+++ SoftEngProject/src/com/terminators/main/MainActivity.java
@@ -0,0 +1,25 @@
+package com.terminators.main;
+
+import com.example.softengproject.R;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity
+{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		addFragment();
+	}
+
+	void addFragment(){
+		getSupportFragmentManager().beginTransaction().add(R.id.fragments_holder,
+				PostsFragment.newInstance("askreddit"))
+				.commit();
+	}
+
+}
