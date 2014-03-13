8fcc075777a194442f20513d2353f589ee18764f
mastash3ff <BSheffield2008@Yahoo.com>
2014-03-13 16:35:59 -0500

    Changed Manifest API levels to get rid of Lint Warning.
    Added Social feature in MainActivity.  Commented it out since it's not
    useful thus far.

--- Zero-DayNews/src/com/android/terminators/MainActivity.java
+++ Zero-DayNews/src/com/android/terminators/MainActivity.java
@@ -10,9 +10,11 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
 /**
@@ -23,75 +25,105 @@ import android.widget.TextView;
  */
 public class MainActivity extends FragmentActivity
 {
-  private TextView titleTxt;
-  private Button rssBtn, redditBtn;
-  private AdView adView;
-  private AdRequest adRequest;
+    private TextView titleTxt;
+    private Button rssBtn, redditBtn;
+    private AdView adView;
+    private AdRequest adRequest;
+    private ShareActionProvider mShareActionProvider;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) 
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		adView = (AdView)findViewById(R.id.adView);
-		adRequest = new AdRequest.Builder().build();
-		adView.loadAd(adRequest);
-		
-		titleTxt = (TextView)findViewById(R.id.appTitle);
-		
-    rssBtn = (Button)findViewById(R.id.rssButton);
-    rssBtn.setOnClickListener(rssListener);
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adView = (AdView)findViewById(R.id.adView);
+        adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+        
+        //TODO uncomment this for social share.  needs specific information.  need to work with later.
+        //Intent sendIntent = new Intent();
+        //sendIntent.setAction(Intent.ACTION_SEND);
+        //sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        //sendIntent.setType("text/plain");
+        //startActivity(Intent.createChooser(sendIntent, getResources().getText(R.string.shareto)));
+
+        titleTxt = (TextView)findViewById(R.id.appTitle);
+
+        rssBtn = (Button)findViewById(R.id.rssButton);
+        rssBtn.setOnClickListener(rssListener);
+
+        redditBtn = (Button)findViewById(R.id.redditButton);
+        redditBtn.setOnClickListener(redditListener);
+    }
+
+    //makes use of custom action bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu items for use in the action bar
+        //MenuInflater inflater = getMenuInflater();
+
+        //inflate menu resource file
+        getMenuInflater().inflate(R.menu.activity_action_bar, menu);
+
+        //Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        //Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+
+        //return true to display menu
+        return true;
+
+        //inflater.inflate(R.menu.activity_action_bar, menu);
+        //return super.onCreateOptionsMenu(menu);
+    }
     
-    redditBtn = (Button)findViewById(R.id.redditButton);
-    redditBtn.setOnClickListener(redditListener);
-	}
+    //used to update shareintent
+    private void setShareIntent (Intent shareIntent)
+    {
+        if (mShareActionProvider != null) 
+        {
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
+    }
 
-	//makes use of custom action bar
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		// Inflate the menu items for use in the action bar
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.activity_action_bar, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
+    OnClickListener rssListener = new OnClickListener()
+    {
+        public void onClick(View v)
+        {
+            Intent intent = new Intent(getApplicationContext(), ITCutiesReaderAppActivity.class);
+            startActivity(intent);
+        }
+    };
 
-  OnClickListener rssListener = new OnClickListener()
-	{
-	  public void onClick(View v)
-	  {
-	    Intent intent = new Intent(getApplicationContext(), ITCutiesReaderAppActivity.class);
-	    startActivity(intent);
-	  }
-	};
-	  
-	OnClickListener redditListener = new OnClickListener()
-  {
-    public void onClick(View v)
-	  {
-	    addFragment();
-	  }
-  };
-    
-  void addFragment()
-  {
-    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction()
-        .add(R.id.fragment_holder, PostFragment.newInstance("technology"));
-    transaction.addToBackStack(null);
-    transaction.commit();
-    titleTxt.setVisibility(View.GONE);
-    rssBtn.setVisibility(View.GONE);
-    redditBtn.setVisibility(View.GONE);
-  }
+    OnClickListener redditListener = new OnClickListener()
+    {
+        public void onClick(View v)
+        {
+            addFragment();
+        }
+    };
 
-  @Override
-  public void onBackPressed()
-  {
-    super.onBackPressed();
-    titleTxt.setVisibility(View.VISIBLE);
-    rssBtn.setVisibility(View.VISIBLE);
-    redditBtn.setVisibility(View.VISIBLE);
-  }
-  
+    void addFragment()
+    {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_holder, PostFragment.newInstance("technology"));
+        transaction.addToBackStack(null);
+        transaction.commit();
+        titleTxt.setVisibility(View.GONE);
+        rssBtn.setVisibility(View.GONE);
+        redditBtn.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
+        titleTxt.setVisibility(View.VISIBLE);
+        rssBtn.setVisibility(View.VISIBLE);
+        redditBtn.setVisibility(View.VISIBLE);
+    }
+
 }
