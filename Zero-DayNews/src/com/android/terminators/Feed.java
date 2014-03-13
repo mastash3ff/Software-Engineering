75728697ac0dc0fde10dd79341b337dbcfd41f27
Zeriam <derrickrb@comcast.net>
2014-03-13 17:59:35 -0500

    Added class Feed.java to Git index.

---
+++ Zero-DayNews/src/com/android/terminators/Feed.java
@@ -0,0 +1,44 @@
+package com.android.terminators;
+
+import java.util.ArrayList;
+
+public class Feed
+{
+  private ArrayList<String> rssFeedList;
+  private ArrayList<String> redditFeedList;
+  private String techCrunch = "http://feeds.feedburner.com/TechCrunch/";
+  private String slashDot = "http://rss.slashdot.org/Slashdot/slashdot/";
+  private String wired = "http://feeds.wired.com/wired27b";
+  private String technology = "technology";
+  
+  public Feed()
+  {
+    rssFeedList = new ArrayList<String>();
+    redditFeedList = new ArrayList<String>();
+    addRssFeed(techCrunch);
+    addRssFeed(slashDot);
+    addRssFeed(wired);
+    addRedditFeed(technology);
+  }
+  
+  public ArrayList<String> getRssFeed()
+  {
+    return rssFeedList;
+  }
+  
+  public ArrayList<String> getRedditFeed()
+  {
+    return redditFeedList;
+  }
+  
+  public void addRssFeed(String feed)
+  {
+    rssFeedList.add(feed);
+  }
+  
+  public void addRedditFeed(String feed)
+  {
+    redditFeedList.add(feed);
+  }
+  
+}
