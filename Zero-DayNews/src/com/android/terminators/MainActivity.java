75ba7b38d75d20fb93b9fdb51e8d5e47c1aaea7d
mastash3ff <BSheffield2008@Yahoo.com>
2014-03-13 19:56:08 -0500

    -Changed the way ad was being called ( no crazy loop) in MainActivity. There is no a Test Ad in place of.  You might need to tweak the settings on the ad to fit the emulator.
    -Removed ad stuff in activity_main.xml.

--- Zero-DayNews/src/com/android/terminators/MainActivity.java
+++ Zero-DayNews/src/com/android/terminators/MainActivity.java
@@ -9,10 +9,12 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
@@ -24,21 +26,19 @@ import android.widget.TextView;
  */
 public class MainActivity extends FragmentActivity
 {
-  private TextView titleTxt;
-  private Button rssBtn, redditBtn;
-  private AdView adView;
-  private AdRequest adRequest;
-  private ShareActionProvider mShareActionProvider;
+    private TextView titleTxt;
+    private Button rssBtn, redditBtn;
+    private AdView adView;
+    private ShareActionProvider mShareActionProvider;
+    private static final String AD_UNIT_ID = "ca-app-pub-5178282085023497/1033225563";
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) 
-  {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-
-    adView = (AdView)findViewById(R.id.adView);
-    adRequest = new AdRequest.Builder().build();
-    adView.loadAd(adRequest);
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        addAd();
 
     /*
     TODO uncomment this for social share.  needs specific information.  need to work with later.
@@ -47,81 +47,144 @@ public class MainActivity extends FragmentActivity
     sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
     sendIntent.setType("text/plain");
     startActivity(Intent.createChooser(sendIntent, getResources().getText(R.string.shareto)));
-     */
+         */
 
-    titleTxt = (TextView)findViewById(R.id.appTitle);
+        titleTxt = (TextView)findViewById(R.id.appTitle);
 
-    rssBtn = (Button)findViewById(R.id.rssButton);
-    rssBtn.setOnClickListener(rssListener);
+        rssBtn = (Button)findViewById(R.id.rssButton);
+        rssBtn.setOnClickListener(rssListener);
 
-    redditBtn = (Button)findViewById(R.id.redditButton);
-    redditBtn.setOnClickListener(redditListener);
-  }
+        redditBtn = (Button)findViewById(R.id.redditButton);
+        redditBtn.setOnClickListener(redditListener);
+    }
 
-  //makes use of custom action bar
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu)
-  {
-    // Inflate the menu items for use in the action bar
-    //MenuInflater inflater = getMenuInflater();
+    //makes use of custom action bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+        
 
-    //inflate menu resource file
+
+        /* used for Share
+    inflate menu resource file
     getMenuInflater().inflate(R.menu.activity_action_bar, menu);
 
-    //Locate MenuItem with ShareActionProvider
+    Locate MenuItem with ShareActionProvider
     MenuItem item = menu.findItem(R.id.menu_item_share);
 
-    //Fetch and store ShareActionProvider
+    Fetch and store ShareActionProvider
     mShareActionProvider = (ShareActionProvider) item.getActionProvider();
 
-    //return true to display menu
+    return true to display menu
     return true;
-  }
-
-  //used to update shareintent
-  private void setShareIntent (Intent shareIntent)
-  {
-    if (mShareActionProvider != null) 
-    {
-      mShareActionProvider.setShareIntent(shareIntent);
+         */
     }
-  }
 
-  OnClickListener rssListener = new OnClickListener()
-  {
-    public void onClick(View v)
+    //used to update shareintent
+    private void setShareIntent (Intent shareIntent)
     {
-      Intent intent = new Intent(getApplicationContext(), ITCutiesReaderAppActivity.class);
-      startActivity(intent);
+        if (mShareActionProvider != null) 
+        {
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
     }
-  };
 
-  OnClickListener redditListener = new OnClickListener()
-  {
-    public void onClick(View v)
+    OnClickListener rssListener = new OnClickListener()
     {
-      addFragment();
+        public void onClick(View v)
+        {
+            Intent intent = new Intent(getApplicationContext(), ITCutiesReaderAppActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    OnClickListener redditListener = new OnClickListener()
+    {
+        public void onClick(View v)
+        {
+            addFragment();
+        }
+    };
+
+    void addFragment()
+    {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_holder, PostFragment.newInstance("technology"));
+        transaction.addToBackStack(null);
+        transaction.commit();
+        titleTxt.setVisibility(View.GONE);
+        rssBtn.setVisibility(View.GONE);
+        redditBtn.setVisibility(View.GONE);
     }
-  };
 
-  void addFragment()
-  {
-    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction()
-        .add(R.id.fragment_holder, PostFragment.newInstance("technology"));
-    transaction.addToBackStack(null);
-    transaction.commit();
-    titleTxt.setVisibility(View.GONE);
-    rssBtn.setVisibility(View.GONE);
-    redditBtn.setVisibility(View.GONE);
-  }
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
+        titleTxt.setVisibility(View.VISIBLE);
+        rssBtn.setVisibility(View.VISIBLE);
+        redditBtn.setVisibility(View.VISIBLE);
+    }
 
-  @Override
-  public void onBackPressed()
-  {
-    super.onBackPressed();
-    titleTxt.setVisibility(View.VISIBLE);
-    rssBtn.setVisibility(View.VISIBLE);
-    redditBtn.setVisibility(View.VISIBLE);
-  }
+    @Override
+    public void onResume() 
+    {
+        super.onResume();
+        if (adView != null) 
+        {
+            adView.resume();
+        }
+    }
 
+    @Override
+    public void onPause() 
+    {
+        if (adView != null)
+        {
+            adView.pause();
+        }
+        super.onPause();
+    }
+
+    /** Called before the activity is destroyed. */
+    @Override
+    public void onDestroy() 
+    {
+        // Destroy the AdView.
+        if (adView != null)
+        {
+            adView.destroy();
+        }
+        super.onDestroy();
+    }
+
+    public void addAd()
+    {
+        // Create an ad.
+        adView = new AdView(this);
+        adView.setAdSize(AdSize.BANNER);
+        adView.setAdUnitId(AD_UNIT_ID);
+
+        // Add the AdView to the view hierarchy. The view will have no size
+        // until the ad is loaded.
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.fragment_holder);
+        layout.addView(adView);
+
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device.
+        AdRequest adRequest = new AdRequest.Builder()
+            .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+            //brandon's emulator test device might need to change to ** if you get problems
+            .addTestDevice("B3EEABB8EE11C2BE770B684D95219ECB")
+            .build();
+
+        // Start loading the ad in the background.
+        adView.loadAd(adRequest);
+        
+        
+    }
 }
