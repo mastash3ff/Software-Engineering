8dfc7cfec9ac5b455fb6160860d53ea9faf3ab7d
mastash3ff <BSheffield2008@Yahoo.com>
2014-03-25 15:45:01 -0500

    -Added new permission to manifest for I/O -Load and Save Feeds are currently working as intended.  May end up encrypting the files to prevent tampering. -Changed MainActivity code dealing with StorageLinks to reflect updated changes

--- Zero-DayNews/src/com/android/terminators/MainActivity.java
+++ Zero-DayNews/src/com/android/terminators/MainActivity.java
@@ -1,6 +1,5 @@
 package com.android.terminators;
 
-import java.util.ArrayList;
 import com.android.terminators.ZeroDayNews.R;
 import com.android.terminators.reddit.RedditBuilder;
 import com.android.terminators.rss.RssBuilder;
@@ -9,6 +8,7 @@ import com.google.android.gms.ads.*;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -16,7 +16,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 /**
  * Zero Day News
@@ -26,178 +25,177 @@ import android.widget.Toast;
  */
 public class MainActivity extends Activity
 {
-  private Button rssBtn, redditBtn, addFeedBtn, configureRssFeedsBtn, configureRedditFeedsBtn;
-  private AdView adView;
-  private static final String AD_UNIT_ID = "ca-app-pub-5178282085023497/1033225563";
+	private Button rssBtn, redditBtn, addFeedBtn, configureRssFeedsBtn, configureRedditFeedsBtn;
+	private AdView adView;
+	private static final String AD_UNIT_ID = "ca-app-pub-5178282085023497/1033225563";
+	static final private String TAG = MainActivity.class.getSimpleName();
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) 
-  {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+	@Override
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    rssBtn = (Button)findViewById(R.id.rssButton);
-    rssBtn.setOnClickListener(rssListener);
+		rssBtn = (Button)findViewById(R.id.rssButton);
+		rssBtn.setOnClickListener(rssListener);
 
-    redditBtn = (Button)findViewById(R.id.redditButton);
-    redditBtn.setOnClickListener(redditListener);
+		redditBtn = (Button)findViewById(R.id.redditButton);
+		redditBtn.setOnClickListener(redditListener);
 
-    addFeedBtn = (Button)findViewById(R.id.addFeedButton);
-    addFeedBtn.setOnClickListener(addFeedListener);
+		addFeedBtn = (Button)findViewById(R.id.addFeedButton);
+		addFeedBtn.setOnClickListener(addFeedListener);
 
-    configureRssFeedsBtn = (Button)findViewById(R.id.configureRssFeedsButton);
-    configureRssFeedsBtn.setOnClickListener(configureRssFeedsListener);
+		configureRssFeedsBtn = (Button)findViewById(R.id.configureRssFeedsButton);
+		configureRssFeedsBtn.setOnClickListener(configureRssFeedsListener);
 
-    configureRedditFeedsBtn = (Button)findViewById(R.id.configureRedditFeedsButton);
-    configureRedditFeedsBtn.setOnClickListener(configureRedditFeedsListener);
-    
-    addAd();
-  }
+		configureRedditFeedsBtn = (Button)findViewById(R.id.configureRedditFeedsButton);
+		configureRedditFeedsBtn.setOnClickListener(configureRedditFeedsListener);
 
-  //makes use of custom action bar
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu)
-  {
-    // Inflate the menu items for use in the action bar
-    MenuInflater inflater = getMenuInflater();
-    inflater.inflate(R.menu.main, menu);
-    return super.onCreateOptionsMenu(menu);
-  }
+		addAd();
+		
+		Log.d(TAG, "onCreate called");
+	}
 
-  //opens the appropriate dialogs when option items are selected
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item)
-  {
-    switch (item.getItemId())
-    {
-      case R.id.action_configureFeeds:
-        FeedManager.getInstance().configureFeeds(this);
-        break;
-      case R.id.action_addFeed:
-        FeedManager.getInstance().addFeed(this);
-        break;
-      case R.id.action_loadFeed:
-        StorageLinks.initializeStorage(getApplicationContext());
-        Toast.makeText(getApplicationContext(), "Feeds Loaded", Toast.LENGTH_SHORT).show();
-        break;
-      case R.id.action_saveFeed:
-        //TODO: this still needs work
-        ArrayList<Feed> feedList = FeedManager.getInstance().getFeedList(Feed.RSS_FEED);
-        for (int i = 4; i < feedList.size(); ++i)
-          StorageLinks.writeStoredFeeds(feedList.get(i).getFeedSite() + " "
-            + Integer.toString(feedList.get(i).getFeedType()) + " " + feedList.get(i).isEnabled().toString());
+	//makes use of custom action bar
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 
-        feedList = FeedManager.getInstance().getFeedList(Feed.REDDIT_FEED);
-        for (int i = 3; i < feedList.size(); ++i)
-          StorageLinks.writeStoredFeeds(feedList.get(i).getFeedSite() + " "
-            + Integer.toString(feedList.get(i).getFeedType()) + " " + feedList.get(i).isEnabled().toString());
+	//opens the appropriate dialogs when option items are selected
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+		case R.id.action_configureFeeds:
+			FeedManager.getInstance().configureFeeds(this);
+			break;
+		case R.id.action_addFeed:
+			FeedManager.getInstance().addFeed(this);
+			break;
+		case R.id.action_loadFeed:
+			StorageLinks.loadStorage(getApplicationContext());
+			break;
+		case R.id.action_saveFeed:
+			StorageLinks.saveFeeds();
+			break;
+		default:
+			break;
+		}
+		return true;
+	}
 
-        Toast.makeText(getApplicationContext(), "Feeds Saved", Toast.LENGTH_SHORT).show();
-        break;
-      default:
-        break;
-    }
-    return true;
-  }
+	OnClickListener rssListener = new OnClickListener()
+	{
+		public void onClick(View v)
+		{
+			Intent intent = new Intent(getApplicationContext(), RssBuilder.class);
+			startActivity(intent);
+		}
+	};
 
-  OnClickListener rssListener = new OnClickListener()
-  {
-    public void onClick(View v)
-    {
-      Intent intent = new Intent(getApplicationContext(), RssBuilder.class);
-      startActivity(intent);
-    }
-  };
+	OnClickListener redditListener = new OnClickListener()
+	{
+		public void onClick(View v)
+		{
+			Intent intent = new Intent(getApplicationContext(), RedditBuilder.class);
+			startActivity(intent);
+		}
+	};
 
-  OnClickListener redditListener = new OnClickListener()
-  {
-    public void onClick(View v)
-    {
-      Intent intent = new Intent(getApplicationContext(), RedditBuilder.class);
-      startActivity(intent);
-    }
-  };
+	OnClickListener addFeedListener = new OnClickListener()
+	{
+		public void onClick(View v)
+		{
+			FeedManager.getInstance().addFeed(MainActivity.this);
+		}
+	};
 
-  OnClickListener addFeedListener = new OnClickListener()
-  {
-    public void onClick(View v)
-    {
-      FeedManager.getInstance().addFeed(MainActivity.this);
-    }
-  };
+	OnClickListener configureRssFeedsListener = new OnClickListener()
+	{
+		public void onClick(View v)
+		{
+			FeedManager.getInstance().configureRssFeeds(MainActivity.this);
+		}
+	};
 
-  OnClickListener configureRssFeedsListener = new OnClickListener()
-  {
-    public void onClick(View v)
-    {
-      FeedManager.getInstance().configureRssFeeds(MainActivity.this);
-    }
-  };
+	OnClickListener configureRedditFeedsListener = new OnClickListener()
+	{
+		public void onClick(View v)
+		{
+			FeedManager.getInstance().configureRedditFeeds(MainActivity.this);
+		}
+	};
 
-  OnClickListener configureRedditFeedsListener = new OnClickListener()
-  {
-    public void onClick(View v)
-    {
-      FeedManager.getInstance().configureRedditFeeds(MainActivity.this);
-    }
-  };
+	@Override
+	public void onResume() 
+	{
+		super.onResume();
+		if (adView != null) 
+			adView.resume();
+		
+		Log.d(TAG, "onPause called");
+	}
 
-  @Override
-  public void onResume() 
-  {
-    super.onResume();
-    if (adView != null) 
-      adView.resume();
-  }
+	@Override
+	public void onPause() 
+	{
+		if (adView != null)
+			adView.pause();
+		super.onPause();
+		
+		Log.d(TAG, "onPause called");
+	}
 
-  @Override
-  public void onPause() 
-  {
-    if (adView != null)
-      adView.pause();
-    super.onPause();
-  }
+	/** Called before the activity is destroyed. */
+	@Override
+	public void onDestroy() 
+	{
+		// Destroy the AdView.
+		if (adView != null)
+			adView.destroy();
+		super.onDestroy();
+		
+		Log.d(TAG, "onDestroy called");
+	}
 
-  /** Called before the activity is destroyed. */
-  @Override
-  public void onDestroy() 
-  {
-    // Destroy the AdView.
-    if (adView != null)
-      adView.destroy();
-    super.onDestroy();
-  }
-  
-  /**
-   * Ads an ad to  fragment_holder
-   */
-  public void addAd()
-  {
-    // Create an ad.
-    adView = new AdView(this);
-    adView.setAdSize(AdSize.BANNER);
-    adView.setAdUnitId(AD_UNIT_ID);
+	/**
+	 * Ads an ad to  fragment_holder
+	 */
+	public void addAd()
+	{
+		// Create an ad.
+		adView = new AdView(this);
+		adView.setAdSize(AdSize.BANNER);
+		adView.setAdUnitId(AD_UNIT_ID);
 
-    // Add the AdView to the view hierarchy. The view will have no size
-    // until the ad is loaded.
-    RelativeLayout layout = (RelativeLayout)findViewById(R.id.activity_main);
+		// Add the AdView to the view hierarchy. The view will have no size
+		// until the ad is loaded.
+		RelativeLayout layout = (RelativeLayout)findViewById(R.id.activity_main);
 
-    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-      RelativeLayout.LayoutParams.WRAP_CONTENT,
-      RelativeLayout.LayoutParams.WRAP_CONTENT);
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+				RelativeLayout.LayoutParams.WRAP_CONTENT,
+				RelativeLayout.LayoutParams.WRAP_CONTENT);
 
-    params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-    params.addRule(RelativeLayout.CENTER_HORIZONTAL);
-    layout.addView(adView, params);
+		params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		params.addRule(RelativeLayout.CENTER_HORIZONTAL);
+		layout.addView(adView, params);
 
-    // Create an ad request. Check logcat output for the hashed device ID to
-    // get test ads on a physical device.
-    AdRequest adRequest = new AdRequest.Builder()
-      .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-      .addTestDevice("B3EEABB8EE11C2BE770B684D95219ECB")
-      .build();
+		// Create an ad request. Check logcat output for the hashed device ID to
+		// get test ads on a physical device.
+		AdRequest adRequest = new AdRequest.Builder()
+		.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+		.addTestDevice("B3EEABB8EE11C2BE770B684D95219ECB")
+		.build();
 
-    // Start loading the ad in the background.
-    adView.loadAd(adRequest);
-  }
+		// Start loading the ad in the background.
+		adView.loadAd(adRequest);
+		
+		Log.d(TAG, "addAd ran");
+	}
 }
--- Zero-DayNews/src/com/android/terminators/storage/StorageLinks.java
+++ Zero-DayNews/src/com/android/terminators/storage/StorageLinks.java
@@ -9,10 +9,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import com.android.terminators.Feed;
 import com.android.terminators.FeedManager;
 import android.content.Context;
 import android.os.Environment;
+import android.util.Log;
 import android.widget.Toast;
 
 /**
@@ -21,188 +24,258 @@ import android.widget.Toast;
  * @author Brandon
  */
 
+/**
+ * @author Brandon Sheffield 
+ * Creation Date: @date
+ *
+ */
 public class StorageLinks implements Storage
 {
-  static private String cacheDirectory = 
-      "/Android/data/com.android.terminators/cache/"; 
-  static final private String fileName = "storagelinks.txt";
-  static final private String filePath = cacheDirectory + fileName;
+	static private String cacheDirectory = 
+			"/Android/data/com.android.terminators/cache/"; 
+	static final private String FILENAME = "storagelinks.txt";
+	static final private String TAG = StorageLinks.class.getSimpleName();
 
-  /*
-   * Make sure the SD Card is available and we can write to it
-   * Once confirmed, create the cache directory if it does not
-   * exist yet
-   */
-
-  static 
-  {
-    if(Environment.getExternalStorageState()
-        .equals(Environment.MEDIA_MOUNTED))
-    {
-      cacheDirectory = Environment.getExternalStorageDirectory()
-          + cacheDirectory;
-      File f=new File(cacheDirectory);
-      f.mkdirs();
-    }
-  }
-
-  /**
-   * Utility method to determine if storagelinks.txt exists on Android Storage
-   * @return
-   */
-  public static Boolean checkIfStorageFileExists()
-  {
-    File f = new File ( getFilepath() );
-
-    if ( f.exists() ) 
-      return true;
-    else
-      return false;
-  }
-
-  /**
-   * Creates a blank links.txt file if it is non-existent.
-   */
-  public static void createFile()
-  {
-    File f = new File( getFilepath() );
-
-    try
-    {
-      f.createNewFile();
-    } catch (IOException e)
-    {
-      e.printStackTrace();
-    }
-  }
-
-  /**
-   * Reads the contents of links.txt file.  Takes only the filename.txt as a string.
-   * @param fileName
-   * 
-   * */
-  public static ArrayList<String> readStoredFeeds( )
-  {
-    //filename parameter will only expect reddit and rss text file
-    File f = new File( getFilepath() );
-    String contents = null;
-    BufferedReader br = null;
-    ArrayList<String> list = new ArrayList<String>();
-
-    //read contents of file if it does exist and return a list of links
-    try
-    {
-      br = new BufferedReader(new FileReader(f));
-
-      while ( (contents = br.readLine() ) != null) 
-      {
-        list.add(contents);
-      }
-    } 
-    catch (FileNotFoundException e)
-    {
-      e.printStackTrace();
-    }
-    catch (IOException e)
-    {
-      e.printStackTrace();
-    } 
-    finally 
-    {
-      try
-      {
-        if (br != null)br.close();
-      } catch (IOException ex) 
-      {
-        ex.printStackTrace();
-      }
-    }
-    return list;
-  }
-
-  /** Checks to see if file exists with Feed links if not creates a file called 
-   * StoredLinks.txt on storage
-   */
-  public static void initializeStorage( Context context )
-  {
-    //reads text file to see what links are saved and returns as a list
-    ArrayList<String> listOfStrings = readStoredFeeds();
-
-    //file checks
-    if ( !StorageLinks.checkIfStorageFileExists() ) 
-    {
-      StorageLinks.createFile();
-    }
-    else
-    {
-      String[] feedData;
-
-      for ( int i = 0; i < listOfStrings.size(); ++i )
-      {
-        //on startup displays what links are already stored and adds them to feed
-        //feedData = listOfStrings.get(i).split("\\s+");
-        feedData = listOfStrings.get(i).split(" ");
-        Toast.makeText(context, feedData[0], Toast.LENGTH_SHORT).show();
-        Toast.makeText(context, feedData[2], Toast.LENGTH_SHORT).show();
-        Toast.makeText(context, feedData[1], Toast.LENGTH_SHORT).show();
-
-        FeedManager.getInstance().addFeed( new Feed( feedData[0], Integer.parseInt(feedData[1]), 
-            Boolean.parseBoolean( feedData[2])));
-      }
-
-      Toast.makeText( context, "Saved Feeds Loaded" , Toast.LENGTH_SHORT).show();
-    }
-  }
-
-  /**Writes to storagelinks.txt
-   * @param link
-   * */
-  public static void writeStoredFeeds(String link)
-  {
-    try
-    {
-      File f = new File( getFilepath() );
-      PrintWriter pw=new PrintWriter( new BufferedWriter(new FileWriter(f, true)));
-      pw.println( link );
-      pw.close();
-    }
-    catch(Exception e) 
-    {
-      e.printStackTrace();
-    }
-  }
+	/*
+	 * Make sure the SD Card is available and we can write to it
+	 * Once confirmed, create the cache directory if it does not
+	 * exist yet
+	 */
+	static 
+	{
+		if(Environment.getExternalStorageState()
+				.equals(Environment.MEDIA_MOUNTED))
+		{
+			cacheDirectory = Environment.getExternalStorageDirectory()
+					+ cacheDirectory;
+			File f = new File( cacheDirectory );
+			f.mkdirs();
+		}
+	}
 
 
-  /**
-   * Brandon's lazy way of deleting the contents of a file.
-   */
-  @Override
-  public void clear()
-  {
-    File f = new File ( getFilepath() );
+	/**
+	 * Utility method to determine if storagelinks.txt exists on Android Storage
+	 * @return
+	 */
+	public static Boolean checkIfStorageFileExists()
+	{
+		File f = new File ( cacheDirectory, FILENAME );
 
-    if ( f.isDirectory() ) 
-      f.delete();
+		if ( f.exists() ) 
+			return true;
+		else
+			return false;
+	}
 
-    createFile();
-  }
 
-  /**
-   * Fetches the Cache Directory which is the system's file path to the applications folder.
-   * @return
-   */
-  public static String getCacheDirectory()
-  {
-    return cacheDirectory;
-  }
+	/**
+	 * Reads the contents of storagelinks.txt file and returns a list of what it read.
+	 * @param FILENAME
+	 * 
+	 * */
+	public static ArrayList<String> readStoredFeeds( )
+	{
+		//filename parameter will only expect reddit and rss text file
+		File f = new File( cacheDirectory, FILENAME );
+		String contents = null;
+		BufferedReader br = null;
+		ArrayList<String> list = new ArrayList<String>();
 
-  public static String getFilename()
-  {
-    return fileName;
-  }
+		//read contents of file if it does exist and return a list of links
+		try
+		{
+			br = new BufferedReader(new FileReader(f));
 
-  public static String getFilepath()
-  {
-    return filePath;
-  }
+			while ( (contents = br.readLine() ) != null) 
+			{
+				list.add(contents);
+			}
+		} 
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+			Log.d(TAG, e.toString());
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			Log.d(TAG, e.toString());
+		} 
+		finally 
+		{
+			try
+			{
+				if (br != null)br.close();
+			} catch (IOException ex) 
+			{
+				ex.printStackTrace();
+				Log.d(TAG, ex.toString());
+			}
+		}
+		
+		//lazy haxxor technique for removing duplicates in file
+		for (String string : list)
+			Log.d(TAG, "list contents before:  " + 	string);
+		
+		Set<String> lStrings = new HashSet<String>(list);
+		list.clear();
+		list.addAll(lStrings);
+		
+		for (String string : list)
+			Log.d(TAG, "list contents after:  " + 	string);
+		
+		return list;
+	}
+
+	/** Checks to see if file exists with Feed links if not creates a file called 
+	 * StoredLinks.txt on storage
+	 * @param context
+	 */
+	public static void loadStorage( Context context )
+	{
+
+		Log.d(TAG, "beginning loadStorage...");
+		//reads text file to see what links are saved and returns as a list
+		ArrayList<String> listOfStrings = readStoredFeeds();
+
+		if ( !StorageLinks.checkIfStorageFileExists() ) 
+		{
+			Toast.makeText(context, "No Saved Feeds Detected on Device" , Toast.LENGTH_SHORT);
+			Log.d(TAG, "tried to load non-existent storagelinks.txt file.");
+		}
+		else
+		{
+			Log.d(TAG, "File exists!  Begin parsing...");
+			String[] feedData;
+
+			for ( int i = 0; i < listOfStrings.size(); ++i )
+			{
+				//on startup displays what links are already stored and adds them to feed
+				feedData = listOfStrings.get(i).split(" ");
+
+				Log.d(TAG, "feedData[" + i + "] contents: " + feedData[i]);
+
+				FeedManager.getInstance().addFeed( new Feed( feedData[0], Integer.parseInt(feedData[1]), 
+						Boolean.parseBoolean( feedData[2])));
+			}
+
+			Toast.makeText( context, "Saved Feeds Loaded" , Toast.LENGTH_SHORT).show();
+			Log.d(TAG, "loadStorage ran");
+		}
+	}
+
+	/**Writes strings from saveFeeds() to storagelinks.txt
+	 * @param link
+	 * */
+	public static void writeStoredFeeds(String link)
+	{
+
+		Log.d(TAG, "beginning writeStoredFeeds...");
+		
+		try
+		{
+			File f = new File( cacheDirectory, FILENAME );
+			PrintWriter pw = new PrintWriter( new BufferedWriter( new FileWriter( f, true )));
+			Log.d(TAG, "writing to file: " + link);
+			pw.println( link );
+			pw.close();
+		}
+		catch(Exception e) 
+		{
+			e.printStackTrace();
+			Log.d(TAG, e.toString());
+		}
+	}
+
+	public static void saveFeeds()
+	{
+		Log.d(TAG, "saveFeeds beginning...");
+
+		ArrayList<Feed> feedList = FeedManager.getInstance().getFeedList(Feed.RSS_FEED);
+
+		for (int i = 4; i < feedList.size(); ++i)
+			StorageLinks.writeStoredFeeds(feedList.get(i).getFeedSite() + " "
+					+ Integer.toString(feedList.get(i).getFeedType()) + " " + feedList.get(i).isEnabled().toString());
+
+		feedList = FeedManager.getInstance().getFeedList(Feed.REDDIT_FEED);
+		
+		for (int i = 0; i < feedList.size(); ++i)
+			Log.d(TAG, "all the reddit feeds: " + feedList.get(i).getFeedSite() );
+
+		for (int i = 3; i < feedList.size(); ++i)
+			StorageLinks.writeStoredFeeds(feedList.get(i).getFeedSite() + " "
+					+ Integer.toString(feedList.get(i).getFeedType()) + " " + feedList.get(i).isEnabled().toString());
+
+		Log.d(TAG, "saveFeeds ran");
+
+	}
+
+	/**
+	 * Clear contents storagelinks.txt 
+	 */
+	@Override
+	public void clear()
+	{
+		File f = new File ( cacheDirectory, FILENAME );
+
+		if ( f.isDirectory() ) 
+			f.delete();
+	}
+
+	/* Checks if external storage is available for read and write */
+	public static boolean isExternalStorageWritable() {
+		String state = Environment.getExternalStorageState();
+		if (Environment.MEDIA_MOUNTED.equals(state)) {
+			return true;
+		}
+		return false;
+	}
+
+	/* Checks if external storage is available to at least read */
+	public static boolean isExternalStorageReadable() {
+		String state = Environment.getExternalStorageState();
+		if (Environment.MEDIA_MOUNTED.equals(state) ||
+				Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Fetches the Cache Directory which is the system's file path to the applications folder.
+	 * @return
+	 */
+	public static String getCacheDirectory()
+	{
+		return cacheDirectory;
+	}
+
+	/**
+	 * Returns the name of file aka storagelinks.txt
+	 * @return
+	 */
+	public static String getFilename()
+	{
+		return FILENAME;
+	}
+	
+	/**
+	 * Returns the tag associated for the log tool
+	 * @return
+	 */
+	public static String getTag()
+	{
+		return TAG;
+	}
+	
+	/**
+	 * Sets the file path to the cache directory.
+	 * @param cacheDirectory
+	 */
+	public static void setCacheDirectory(String cacheDirectory)
+	{
+		StorageLinks.cacheDirectory = cacheDirectory;
+	}
+
 }  
